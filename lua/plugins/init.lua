1ec37f54ad7e2a9882bb65a6ab430e06a9695b56
kielsun <kielsun@protonmail.com>
2024-05-14 14:14:33 +0800

    main my_nvim

---
+++ lua/plugins/init.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- uncomment for format on save
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "bash",
+        "html",
+        "css",
+        "cpp",
+        "c",
+        "python",
+      },
+    },
+  },
+}
